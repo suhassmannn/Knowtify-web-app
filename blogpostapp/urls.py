7fe184a2036aea7aa3f97af48c368ba42aaa176f
suhassmannn <suhaas0000@gmail.com>
2024-06-26 21:00:19 +0530

    update 1

--- blogpostapp/urls.py
+++ blogpostapp/urls.py
@@ -4,12 +4,14 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
-                    PostDeleteView
+                    PostDeleteView,
+                    UserPostListView
                     )
 
 urlpatterns = [
     path('', PostListView.as_view() , name = 'blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view() , name = 'post-detail'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view() , name = 'post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view() , name = 'post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view() , name = 'post-delete'),
--- blogpostapp/views.py
+++ blogpostapp/views.py
@@ -6,6 +6,8 @@ from django.views.generic import (ListView,
                                   UpdateView,
                                   DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 context = {
     'posts' : Post.objects.all(),
@@ -21,6 +23,17 @@ class PostListView(ListView):
     # by default refers to a post as object. so instead of changing the name 
     # in template new name can be given to refer it by.
     ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
