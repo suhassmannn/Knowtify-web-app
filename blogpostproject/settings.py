a0c029a76269c02a03f2f122f6aaf7b9980ab2b0
suhassmannn <suhaas0000@gmail.com>
2024-06-27 14:37:34 +0530

    complete

--- blogpostproject/settings.py
+++ blogpostproject/settings.py
@@ -142,5 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('suhaas0000@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('nic.teran#03')
+EMAIL_HOST_USER = 'suhaas0000@gmail.com'
+EMAIL_HOST_PASSWORD = 'rmiujgjajzcgemev'
+
