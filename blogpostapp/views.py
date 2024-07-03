f7a310853d23c3880e9693ba2810d998e24421ed
suhassmannn <suhaas0000@gmail.com>
2024-07-03 18:13:15 +0530

    small changes

--- blogpostapp/views.py
+++ blogpostapp/views.py
@@ -29,7 +29,7 @@ class UserPostListView(ListView):
     model = Post
     template_name = 'user_posts.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 5 
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
